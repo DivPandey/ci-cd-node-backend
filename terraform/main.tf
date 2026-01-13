6124848a1fd5cc9b03a3867b08effe1851ed8c49
DivPandey <divyanshu1106@gmail.com>
2026-01-14 03:37:13 +0530

    Add Terraform infrastructure code and update gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ci_cd_server" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "ci-cd-node-server"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name = "allow_ssh_http"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
